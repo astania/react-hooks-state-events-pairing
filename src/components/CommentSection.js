8888e9c84b564bb20f4f696a7b88f0a6ca4cc791
+ <+>
2022-02-02 15:19:17 -0500

    Done.

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Comment from './Comment'
 
 function CommentSection({ comments }) {
+    const [isCommentShown, toggleShowComment] = useState(true)
+
+    const toggleComments = () => {
+        let showComment = !isCommentShown
+        toggleShowComment(showComment)
+        console.log(showComment)
+    }
+
     return (
         <div>
+            <button onClick={toggleComments}>{isCommentShown ? 'Hide Comments' : 'Show Comments'}</button>
             <h3>{comments.length} Comments</h3>
-            {comments.map(comment => <Comment key={comment.id} user={comment.user} comment={comment.comment}/>)}
+            {isCommentShown ? comments.map(comment => <Comment key={comment.id} user={comment.user} comment={comment.comment} />) : null}
         </div>
     )
 
--- src/components/VideoData.js
+++ src/components/VideoData.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function VideoData({views, createdAt, upvotes, downvotes}){
-    return(
+
+function VideoData({ views, createdAt, upvotes, downvotes }) {
+    const [upVoteCount, addOneUpVote] = useState(upvotes)
+    const [downVoteCount, addOneDownVote] = useState(downvotes)
+
+    const handleUpVote = () => {
+        const plusOneVote = upVoteCount + 1
+        addOneUpVote(plusOneVote)
+    }
+
+    const handleDownVote = (event) => {
+        const plusOneVote = downVoteCount + 1
+            addOneDownVote(plusOneVote)
+
+    }
+
+    return (
         <div>
-        <p>{views} views | uploaded on {createdAt}</p>
-        <button>{upvotes}👍</button>
-        <button>{downvotes}👎</button>
+            <p>{views} views | uploaded on {createdAt}</p>
+            <button onClick={handleUpVote} >{upVoteCount}👍</button>
+            <button onClick={handleDownVote}>{downVoteCount}👎</button>
 
         </div>
 
